4d7b4793bb6a4fe231816be3ebfd8243a701ce5d
Olufisayo Bamidele <fisiwizy@gmail.com>
2021-02-25 09:21:30 +0100

    implement polling

--- client/src/controllers/conversation-controller.ts
+++ client/src/controllers/conversation-controller.ts
@@ -1,27 +1,33 @@
-
-import { IMessage } from '../../../types/models';
-import { Conversation } from '../API/conversation';
+import { IMessage } from "../../../types/models";
+import { Conversation } from "../API/conversation";
 
 export class ConversationController {
   constructor(private api: Conversation) {}
 
   initConversation(conversationId: string) {
-    return this.api.initConversation(conversationId)
-      .then(({data: { conversation }}) => conversation);
+    return this.api
+      .initConversation(conversationId)
+      .then(({ data: { conversation } }) => conversation);
   }
 
   getMyConversations() {
-    return this.api.getMyConversations()
-      .then(({data: { conversations }}) => conversations);
+    return this.api
+      .getMyConversations()
+      .then(({ data: { conversations } }) => conversations);
   }
 
   getConversationMessages(conversationId: string) {
-    return this.api.getConversationMessages(conversationId)
-      . then(({data: { chatMessages }}) => chatMessages);
+    return this.api
+      .getConversationMessages(conversationId)
+      .then(({ data: { chatMessages } }) => chatMessages);
   }
 
-  createConversationMessage(conversationId: string, message: Partial<IMessage>) {
-    return this.api.createMessage(conversationId, message)
-      . then(({data: { chatMessage }}) => chatMessage);
+  createConversationMessage(
+    conversationId: string,
+    message: Partial<IMessage>
+  ) {
+    return this.api
+      .createMessage(conversationId, message)
+      .then(({ data: { chatMessage } }) => chatMessage);
   }
 }
